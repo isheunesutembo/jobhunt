5e7f210112d247002551202cc1cf4b2409a975f9
isheunesu tembo <isheunesu48@gmail.com>
2024-08-09 12:23:36 +0300

    added user repository and user controller

--- lib/controllers/authcontroller.dart
+++ lib/controllers/authcontroller.dart
@@ -1,15 +1,11 @@
-
-
-import 'dart:ffi';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:fpdart/fpdart.dart';
 import 'package:jobhunt/models/usermodel.dart';
 import 'package:jobhunt/providers/currentusernotifier.dart';
-import 'package:jobhunt/providers/httpprovider.dart';
 import 'package:jobhunt/repository/authrepository.dart';
 import 'package:jobhunt/repository/localauthrepository.dart';
+import 'package:jobhunt/util/utils.dart';
 final authControllerProvider=AsyncNotifierProvider<AuthContoller,UserModel>(AuthContoller.new);
 class AuthContoller extends AsyncNotifier<UserModel>{
  late AuthRepository _authRepository;
@@ -26,44 +22,8 @@ class AuthContoller extends AsyncNotifier<UserModel>{
  Future<void> logInWithEmailPassword(String email,String password,BuildContext context)async{
  final user=await _authRepository.logInWithEmailPassword(email, password);
   final val= switch(user){
-    Left(value:final l)=> AlertDialog(
-        title: const Text('JobHunt'),
-        content:  SingleChildScrollView(
-          child: ListBody(
-            children: <Widget>[
-             
-              Text(l.message.toString()),
-            ],
-          ),
-        ),
-        actions: <Widget>[
-          TextButton(
-            child: const Text('Ok'),
-            onPressed: () {
-              Navigator.of(context).pop();
-            },
-          ),
-        ],
-      ),
-    Right(value:final r)=>AlertDialog(
-        title: const Text('JobHunt '),
-        content:  SingleChildScrollView(
-          child: ListBody(
-            children: <Widget>[
-             const Text('LogIn Successful!'),
-              Text("Welcome ${r.username}"),
-            ],
-          ),
-        ),
-        actions: <Widget>[
-          TextButton(
-            child: const Text('Ok'),
-            onPressed: () {
-              Navigator.of(context).pop();
-            },
-          ),
-        ],
-      ),
+    Left(value:final l)=> showSnackBar(context,l.toString()),
+    Right(value:final r)=>Navigator.pushNamed(context, "/mainscreen")
   };
  }
 
@@ -71,46 +31,9 @@ class AuthContoller extends AsyncNotifier<UserModel>{
   final user=await _authRepository.registerWithEmailPassword(email, username, password);
 
   final val=switch(user){
-    Left(value: final l)=>AlertDialog(
-        title: const Text('JobHunt'),
-        content:  SingleChildScrollView(
-          child: ListBody(
-            children: <Widget>[
-             
-              Text(l.message.toString()),
-            ],
-          ),
-        ),
-        actions: <Widget>[
-          TextButton(
-            child: const Text('Ok'),
-            onPressed: () {
-              Navigator.of(context).pop();
-            },
-          ),
-        ],
-      ),
-      Right(value:final r)=>
-      AlertDialog(
-        title: const Text('JobHunt'),
-        content:  SingleChildScrollView(
-          child: ListBody(
-            children: <Widget>[
-             
-              const Text('Registeration Successful!'),
-              Text("Welcome ${r.username}")
-            ],
-          ),
-        ),
-        actions: <Widget>[
-          TextButton(
-            child: const Text('Ok'),
-            onPressed: () {
-              Navigator.of(context).pop();
-            },
-          ),
-        ],
-      ),
+    Left(value: final l)=>showSnackBar(context,l.toString()),
+      Right(value:final r)=>Navigator.pushNamed(context, "/signinscreen")
+    
   };
 
  }
